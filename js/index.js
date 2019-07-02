96413a3bc746a88be01b850b357097951ecad639
ngot <zhuanghengfei@gmail.com>
2019-07-02 23:36:24 +1000

    init

---
+++ js/index.js
@@ -0,0 +1,67 @@
+window.onload = init;
+
+function template(tpl, option = {}) {
+  if (tpl && typeof tpl === "string") {
+    const regx = /{{([a-zA-Z\-]+)}}/g;
+    return tpl.replace(regx, (...argv) => {
+      return option[argv[1]];
+    });
+  } else {
+    return tpl;
+  }
+}
+
+const myCalendar = {
+  header: "五月廿九 乙亥年 【猪年】 庚午月 乙亥日",
+  "month-zh": "七月",
+  "month-en": "JULY",
+  week: "周一",
+  day: 1,
+  title: "忌大而无当",
+  sentence:
+    "如果我还有什么剩下要做的事情，那就是一棵草的事情，一粒虫的事情，一片云的事情。",
+  author: "作家，刘亮程 《一个人的村庄》",
+  logo: "喵喵日历"
+};
+
+const tpl = `
+<div id="header" class="cal-header">
+    {{header}}
+</div>
+<div id="body" class="cal-body">
+    <div class="cal-month-week">
+        <div id="month-zh" class="cal-month-zh">
+            {{month-zh}}
+        </div>
+        <div id="month-en" class="cal-month-en">
+            {{month-en}}
+        </div>
+        <div id="week" class="cal-week">
+            {{week}}
+        </div>
+    </div>
+    <div id="day" class="cal-day">
+        {{day}}
+    </div>
+    <div id="title" class="cal-title">
+        {{title}}
+    </div>
+</div>
+<div id="footer" class="cal-footer">
+    <div id="sentence" class="cal-sentence">
+        {{sentence}}
+    </div>
+    <div>
+    <div id="author" class="cal-author">
+        {{author}}
+    </div>
+    <div id="logo" class="cal-logo">{{logo}}</div>
+    </div>
+</div>
+`;
+
+function init() {
+  const container = document.getElementById("calendar-container");
+  const content = template(tpl, myCalendar);
+  container.innerHTML = content;
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# calendar
+
+## Dev
+
+```shell
+npm i @fibjs/static
+```
+
+```shell
+static .
+```
+
+then visit `http://127.0.0.1:8080/`
